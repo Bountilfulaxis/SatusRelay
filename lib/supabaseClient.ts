91d6487a0a567e5fc5ff4386aeb8d1b0b016f541
Bountilfulaxis <mgough1985@googlemail.com>
2025-07-18 14:53:07 +0100

    Debug: Hardcode Supabase URL

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// These lines get the secret keys from your Netlify settings
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseUr1 = 'https://razluzabxexcajfclfxl.supabase.co'
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// This creates one, single, sharable client for your whole app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
